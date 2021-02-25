92676bfbfb0d2d424348fc36675fff657d24d537
Gavin Crowley <gavincrowley@gmail.com>
2021-02-25 19:42:21 +0400

    4 build style buttons

--- components/atoms/button/button.jsx
+++ components/atoms/button/button.jsx
@@ -1,7 +1,23 @@
+import { func, node, string } from 'prop-types';
 import React from 'react';
+import StyledButton, { StyledLinkButton } from './button.styles';
 
-const Button = ({children}) => {
-    return ( <button>{children}</button> );
+
+
+const Button = ({children, href, onClick, variant}) => {
+   if (!href) return ( <StyledButton onClick={onClick} variant={variant}>{children}</StyledButton> );
+   return <StyledLinkButton href={href} variant={variant}>{children}</StyledLinkButton>
 }
+
+Button.propTypes = {
+    children: node.isRequired,
+    href: string,
+    onClick: func,
+    variant: string
+}
+
+Button.defaultProps = {
+    variant: "primary"
+};
  
 export default Button ;
--- components/atoms/button/button.stories.js
+++ components/atoms/button/button.stories.js
@@ -1,9 +1,16 @@
 import React from 'react';
-// import { Button } from '@storybook/react/demo'
 import Button from './button'
 
-export const text = () => (
-    <Button>My Button</Button>
+export const basicButton = () => (
+    <Button variant="secondary">My basic Button</Button>
+);
+
+export const functionButton = () => (
+    <Button onClick={() => alert('Hello')}>Function Button</Button>
+);
+
+export const linkedButton = () => (
+    <Button href="/route">Linked Button</Button>
 );
 
 export default {
