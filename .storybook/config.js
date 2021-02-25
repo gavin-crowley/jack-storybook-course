7114fcac263fadad7a90416fcfd37daffb677f20
Gavin Crowley <gavincrowley@gmail.com>
2021-02-25 18:38:24 +0400

    2 basic button story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,3 @@
 import { configure } from "@storybook/react";
 
-configure(require.context("../stories", true, /\.stories\.js$/), module);
+configure(require.context("../components", true, /\.stories\.js$/), module);
---
+++ components/atoms/button/button.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Button } from '@storybook/react/demo'
+import Button from './button'
+
+export const text = () => (
+    <Button>My Button</Button>
+);
+
+export default {
+    component: Button,
+    title: "Button"
+};
