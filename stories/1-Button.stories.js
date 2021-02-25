7bd4e1e679353d7d2de746c74aef5c845fa4fdb1
Gavin Crowley <gavincrowley@gmail.com>
2021-02-25 17:13:38 +0400

    first commit

---
+++ stories/1-Button.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from '@storybook/react/demo'
+
+export default {
+    title: "Button"
+};
+
+export const text = () => (
+    <Button>Just a Button</Button>
+);
